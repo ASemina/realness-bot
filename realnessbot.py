912977aca667313cd1a7643814e534847ee80da3
ASemina <34461262+ASemina@users.noreply.github.com>
2018-06-30 02:21:29 -0400

    Update bot script

--- realnessbot.py
+++ realnessbot.py
@@ -15,8 +15,40 @@ import requests
 import time
 import json
 
-last_read = ""
-i = 0
+
+##################################################################
+
+# Bad Words
+vwords = []
+bwords = []
+awords = []
+sewords = []
+sawords = []
+cwords = []
+anywords = []
+##################################################################
+
+# Good Words
+vwords2 = []
+bwords2 = []
+awords2 = []
+sewords2 = []
+sawords2 = []
+cwords2 = []
+anywords2 = []
+##################################################################
+
+#load id -> names
+with open("people.json", 'r') as f:
+    file = f.readlines()
+    people = json.loads(file[0])
+##################################################################
+
+#keeps track of the last message checked and i is a 
+with open("last_read.json", 'r') as f:
+    file = f.readlines()
+    last_read = json.loads(file[0])["last_read"]
+##################################################################
 
 with open("auth.json", 'r') as f:
     file = f.readlines()
@@ -24,46 +56,101 @@ with open("auth.json", 'r') as f:
 
 request_params = {'token': auth["token"]}
 bot_params = {'text':'','bot_id': auth["bot_id"]}
-
-
+##################################################################
 
 with open("realness.json", 'r') as f:
     file = f.readlines()
     tallies = json.loads(file[0])
-
-
+##################################################################
+    
+def v(word):
+    if word in vwords:
+        tallies["Victor"] -= 1
+    elif word in vwords2:
+        tallies["Victor"] += 1
+        
+def c(word):
+    if word in cwords:
+        tallies["Carter"] -= 1
+    elif word in cwords2:
+        tallies["Carter"] += 1
+        
+def a(word):
+    if word in awords:
+        tallies["Alex"] -= 1
+    elif word in awords2:
+        tallies["Alex"] += 1
+        
+def se(word):
+    if word in sewords:
+        tallies["Sean"] -= 1
+    elif word in sewords2:
+        tallies["Sean"] += 1
+        
+def sa(word):
+    if word in sawords:
+        tallies["Sam"] -= 1
+    elif word in sawords2:
+        tallies["Sam"] += 1
+        
+def b(word):
+    if word in bwords:
+        tallies["Bryan"] -= 1
+    elif word in bwords2:
+        tallies["Bryan"] += 1
+        
+def anyone(word):
+    if word in anywords:
+        tallies["Victor"] -= 1
+        tallies["Carter"] -= 1
+        tallies["Alex"] -= 1
+        tallies["Sean"] -= 1
+        tallies["Sam"] -= 1
+        tallies["Bryan"] -= 1
+    elif word in anywords2:
+        tallies["Victor"] += 1
+        tallies["Carter"] += 1
+        tallies["Alex"] += 1
+        tallies["Sean"] += 1
+        tallies["Sam"] += 1
+        tallies["Bryan"] += 1
+##################################################################
+        
 def analyze(message):
     if message['id'] == last_read:
         return True
-#    print(message)
     words = message['text'].split()
     for word in words:
-        if word.lower() == "holmdell":
-            tallies["Victor"] += 5
-    
+        v(word)
+        c(word)
+        se(word)
+        sa(word)
+        a(word)
+        b(word)
+        anyone(word)
     with open("realness.json", 'w') as f:
         f.write(json.dumps(tallies))
     return False        
+##################################################################
 
-
+i = 0
 while(i < 20):
-#    response_post = requests.post('https://api.groupme.com/v3/bots/post', bot_params)
-#    if (response_post.headers['Status'] == "202 Accepted"):
        
-        response_messages = requests.get('https://api.groupme.com/v3/groups/30589429/messages', params = request_params).json()['response']['messages']
-        for message in response_messages:
-            check = analyze(message)
-            if check:
-                break
-        message = response_messages[0]
-        last_read = message['id']
-        
-        print(tallies)
-        i += 1
-        time.sleep(5)
+    response_messages = requests.get('https://api.groupme.com/v3/groups/10908178/messages', params = request_params).json()['response']['messages']
+    for message in response_messages:
+        check = analyze(message)
+        if check:
+            break
+    message = response_messages[0]
+    last_read = message['id']
+    i += 1
+    time.sleep(5)
+##################################################################
 
-
-            
+#keeps track of the last message checked
+with open("last_read.json", 'w') as f:
+    f.write(json.dumps(file[0]))
+##################################################################
     
     
     
